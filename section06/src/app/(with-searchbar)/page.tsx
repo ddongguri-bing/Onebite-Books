10efc93c7ebead36acf84e21adb660e97adbf0ea
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-19 17:07:38 +0900

    ✨ 컴포넌트 스트리밍 적용하기

--- section06/src/app/(with-searchbar)/page.tsx
+++ section06/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,8 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { Delay } from "@/util/delay";
+import { Suspense } from "react";
 
 // export const dynamic = ""; -> 정말 특별한 경우가 아니라면 권장되지 않음!
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
@@ -10,6 +12,7 @@ import { BookData } from "@/types";
 // 4. error : 페이지를 강제로 Static 페이지로 설정 (동적 함수와 같이 설정하면 안되는 이유가 있다면 빌드 오류를 발생)
 
 async function AllBooks() {
+  await Delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -28,6 +31,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await Delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -45,16 +49,22 @@ async function RecoBooks() {
   );
 }
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,21 +1,12 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { Delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{
-    q?: string;
-  }>;
-}) {
+async function SearchResult({ q }: { q: string }) {
   await Delay(1500);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${
-      (
-        await searchParams
-      ).q
-    }`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" } // 검색 결과는 캐싱되어 한 번 검색되었던 검색 결과는 조금 더 빠르게 렌더링(최적화)
   );
   if (!response.ok) {
@@ -28,5 +19,19 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense key={searchParams.q || ""} fallback={<div>Loading...</div>}>
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
